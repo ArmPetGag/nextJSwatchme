f911e587f25e87868c0fdcd48e6d52acbf50496d
Armen Petrosyan <armpetgag@gmail.com>
2022-09-14 10:53:46 +0300

    first

---
+++ components/Hero.js
@@ -0,0 +1,20 @@
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Hero = () => {
+  return (
+    <div className='text-center bg-white pb-10'>
+      <div className="w-60 mx-auto">
+        <Image src={"/hero_cinema.png"} width={280} height={220} layout="responsive" />
+      </div>
+      <h1 className='text-2xl text-gray-600 uppercase font-bold'>Welcome to WatchMe</h1>
+      <p className='text-gray-700  '>Produce FILM featuer,TELEVISION and GAME.</p>
+      <Link href={'/contact'}>
+        <button className='bg-gray-700 text-white py-3 px-6 rounded text-sm mt-4'>CONTACT US</button>
+      </Link>
+    </div>
+  )
+}
+
+export default Hero
---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+
+import Meta from './../components/Meta';
+
+const contact = () => {
+  return (
+    <div className='text-center mt-20'>
+      <Meta title="Contact US" />
+      <h1 className='text-4xl font-bold'>CONTACT US </h1>
+      <h3 className='text-2xl'> +374 98 42 78 87</h3>
+      <h3 className='text-2xl '>armpetgag@gmail.com</h3>
+    </div>
+  )
+}
+
+export default contact
